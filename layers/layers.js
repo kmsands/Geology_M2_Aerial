d17a880d330218579e7baab05035cfe7cc47bee7
kmsands <ddippolito@kmsands.com.au>
2024-06-05 12:14:59 +0800

    push

---
+++ layers/layers.js
@@ -0,0 +1,30 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            //'type': 'base',
+            'opacity': 0.600000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+var lyr_site_comp_may_2024_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "site_comp_may_2024",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/site_comp_may_2024_1.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [13686318.745659, -1973799.047661, 13690974.580305, -1969212.879783]
+                            })
+                        });
+
+lyr_GoogleSatellite_0.setVisible(true);lyr_site_comp_may_2024_1.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_site_comp_may_2024_1];
